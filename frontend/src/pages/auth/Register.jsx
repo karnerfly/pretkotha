2f7afea1c5ea24fba0a7e61dd8bd95343432b693
Sulagna Pradhan <152047288+Sulagna-pradhan@users.noreply.github.com>
2025-03-09 15:32:18 +0000

    modified:   frontend/src/pages/auth/Register.jsx

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
@@ -78,14 +80,19 @@ const RegisterForm = () => {
     <div className="bg-gray-50 text-gray-800 min-h-screen">
       {/* Breadcrumb Navigation */}
       <div className="container mx-auto px-6 py-4">
-        <nav className="text-gray-600 text-sm">
-          <Link to="/" className="hover:text-primary-600">
+        <nav className="text-gray-600 text-sm flex items-center">
+          <Link to="/" className="hover:text-primary-600 flex items-center">
+          <FontAwesomeIcon icon={faHome} className="mr-1" />
             Home
           </Link>
           <span className="mx-2">/</span>
-          <span className="text-primary-700 font-semibold">Register</span>
+          <span className="text-primary-700 font-semibold flex items-center">
+          <FontAwesomeIcon icon={faUserPlus} className="mr-1" />
+          Register
+          </span>
         </nav>
-      </div>
+    </div>
+
 
       {/* Main Content */}
       <div className="container mx-auto px-6 py-16">
