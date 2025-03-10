e134c5b65d22e9035f11b267a42655ef2b2ad5a9
karnerfly <toufique26ajay@gmail.com>
2025-03-11 00:52:10 +0530

    fix public route redirection, fix links path in user dropdown

--- frontend/src/components/user/DbHeader.jsx
+++ frontend/src/components/user/DbHeader.jsx
@@ -36,7 +36,7 @@ const Header = ({ isSidebarOpen, toggleSidebar }) => {
     <header className="shadow-lg dark:bg-gray-800 bg-white transition-colors duration-300">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         {/* Logo */}
-        <Link to="/dashboard" className="flex items-center space-x-2">
+        <Link to="" className="flex items-center space-x-2">
           <div className="w-10 h-10 bg-indigo-600 rounded-lg flex items-center justify-center">
             <span className="text-white text-xl font-bold">D</span>
           </div>
@@ -105,14 +105,14 @@ const Header = ({ isSidebarOpen, toggleSidebar }) => {
                   </p>
                 </div>
                 <Link
-                  to="/dashboard/profile"
+                  to="profile"
                   className="px-4 py-2 text-sm dark:text-gray-200 hover:dark:bg-gray-700 text-gray-700 hover:bg-gray-100 flex items-center"
                 >
                   <FontAwesomeIcon icon={faUser} className="mr-3 text-indigo-500" />
                   Profile
                 </Link>
                 <Link
-                  to="/dashboard/settings"
+                  to="settings"
                   className="px-4 py-2 text-sm dark:text-gray-200 hover:dark:bg-gray-700 text-gray-700 hover:bg-gray-100 flex items-center"
                 >
                   <FontAwesomeIcon icon={faCog} className="mr-3 text-indigo-500" />
